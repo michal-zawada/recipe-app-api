671e26bac4524685b76d78663e03c64c585900a5
michal-zawada <zawada.jm@gmail.com>
2024-02-21 13:21:04 +0100

    Created core app

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x, y):
+    """AAdd x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subract x from y and return result."""
+    return y - x
