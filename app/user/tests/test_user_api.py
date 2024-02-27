f2a842bc7311818ccb190fe4084b8ab490462145
michal-zawada <zawada.jm@gmail.com>
2024-02-27 16:59:20 +0100

    corrected errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -55,9 +55,9 @@ class PublicUserApiTests(TestCase):
             'password': 'pw',
             'name': 'Test name',
         }
-        rest = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(rest.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
