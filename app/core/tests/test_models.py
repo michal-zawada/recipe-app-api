a6c2bdf708bef55229f7877dedb1c939cecfdc1e
michal-zawada <zawada.jm@gmail.com>
2024-02-28 23:43:37 +0100

    added recipe api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,13 @@
 """
 Tests for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
 
 class ModelTests(TestCase):
     """Test models."""
@@ -35,7 +40,7 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
         """Test creating a superuser."""
@@ -46,3 +51,23 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description.',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+
+
+
